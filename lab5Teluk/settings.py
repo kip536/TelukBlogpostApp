d445ca63820f80561ea1b26c3c7fd13d98725221
kip536 <duncankipkorir99@gmail.com>
2023-12-03 23:14:01 +0300

    Added new apps and templates

--- lab5Teluk/settings.py
+++ lab5Teluk/settings.py
@@ -139,3 +139,9 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'login'
+
+LOGIN_URL = 'login'
